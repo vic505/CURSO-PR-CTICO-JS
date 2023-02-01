572f2cc90e8128b539c99a7b5a52df4901eb3f58
vic505 <juarez.miranda.regina.victoria@gmail.com>
2023-01-31 19:42:59 -0600

    MENU HAMBURGUESA Y MEDIA QUERIES

--- fusión-Curso-Frontend/main.js
+++ fusión-Curso-Frontend/main.js
@@ -1,8 +1,15 @@
 const menuEmail=document.querySelector('.navbar-email');
 const desktopMenu=document.querySelector('.desktop-menu');
+const menuHambIcon=document.querySelector('.hambIcon');
+const mobileMenu=document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click',toggleMenu);
+menuHambIcon.addEventListener('click',toggleMobileMenu);
+
 function toggleMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
 
